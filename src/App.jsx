6a0454b6a7aabe2c89c069af64acfb77ad6db3a0
Madhan M <166613405+Madhanmohan11@users.noreply.github.com>
2025-09-22 22:55:54 +0530

    Update App.jsx
    x

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import Header from "./components/Header";
-import Table_1 from "./components/Table_1";
-import Table_2 from "./components/Table_2";
-import Table_3 from "./components/Table_3";
-import Table_4 from "./components/Table_4";
-import Table_5 from "./components/Table_5";
-import Table_6 from "./components/Table_6";
-import Table_7 from "./components/Table_7";
+// import Header from "./components/Header";
+// import Table_1 from "./components/Table_1";
+// import Table_2 from "./components/Table_2";
+// import Table_3 from "./components/Table_3";
+// import Table_4 from "./components/Table_4";
+// import Table_5 from "./components/Table_5";
+// import Table_6 from "./components/Table_6";
+// import Table_7 from "./components/Table_7";
+import Vr from "./components/vr";   // ✅ changed name
 
 import LanguageToggleButton from "./components/LanguageToggleButton";
 
@@ -19,15 +20,16 @@ function App() {
 
   return (
     <div className="bg-gray-100 min-h-screen p-4 relative overflow-x-hidden">
-      <Header lang={lang} />
+{/*       <Header lang={lang} /> */}
       <LanguageToggleButton lang={lang} onToggle={handleLanguageToggle} />
-      <Table_1 lang={lang} />
+{/*       <Table_1 lang={lang} />
       <Table_2 lang={lang} />
       <Table_3 lang={lang} />
       <Table_4 lang={lang} />
       <Table_5 lang={lang} />
       <Table_6 lang={lang} />
-      <Table_7 lang={lang} />
+      <Table_7 lang={lang} /> */}
+      <Vr lang={lang} />   {/* ✅ fixed usage */}
     </div>
   );
 }
